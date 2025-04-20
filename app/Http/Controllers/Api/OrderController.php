c4e27c28969df86edad480e9f8158a85910db739
andry06 <andri.suryono@gmail.com>
2025-04-20 08:37:47 +0700

    perubahan order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -92,4 +92,84 @@ class OrderController extends Controller
             'data' => $order
         ]);
     }
+
+    public function show(Order $order)
+    {
+        $order->load('orderProducts.product', 'paymentMethod');
+
+        $order->orderProducts->transform(function ($item) {
+            return [
+                'product_id' => $item->product_id,
+                'product_name' => $item->product->name ?? '-',
+                'quantity' => $item->quantity ?? 0,
+                'unit_price' => $item->unit_price ?? 0,
+            ];
+        });
+
+
+        return response()->json($order);
+    }
+
+    public function update(Request $request, Order $order)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'nullable|email',
+            'gender' => 'nullable|in:male,female',
+            'birthday' => 'nullable|date',
+            'phone' => 'nullable|string',
+            'total_price' => 'required|numeric',
+            'notes' => 'nullable|string',
+            'payment_method_id' => 'required|exists:payment_methods,id',
+            'items' => 'required|array',
+            'items.*.product_id' => 'required|exists:products,id',
+            'items.*.quantity' => 'required|integer|min:1',
+            'items.*.unit_price' => 'required|integer|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ada kesalahan validasi',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        foreach ($request->items as $item) {
+            $product = Product::find($item['product_id']);
+            if (!$product || $product->stock < $item['quantity']) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Stok produk kosong : '.$product->name
+                ], 422);
+            }
+        }
+
+        $order->update($request->only([
+            'name',
+            'email',
+            'gender',
+            'phone',
+            'total_price',
+            'notes',
+            'payment_method_id',
+            'paid_amount',
+            'change_amount'
+        ]));
+
+        foreach ($request->items as $item) {
+            $order->orderProducts()->create([
+                'product_id' => $item['product_id'],
+                'quantity' => $item['quantity'],
+                'unit_price' => $item['unit_price']
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sukses melakukan order.',
+            'data' => $order
+        ]);
+    }
+
 }
